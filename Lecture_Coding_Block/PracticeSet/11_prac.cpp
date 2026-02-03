e28cb3d7398ddf28efc74f2aa5eca53ec3590e93
kanishka-80 <kanishkagupta0802@gmail.com>
2026-02-03 16:55:41 +0530

    done

---
+++ Lecture_Coding_Block/PracticeSet/11_prac.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,sum=0;
+    cin>>n;
+    while (n!=0)
+    {
+         int r=n%10;
+        sum=sum+r;
+        n=n/10;
+        
+    }
+    cout<<sum;
+    cout<<abs(sum);//for absolute value
+
+return 0;
+}
