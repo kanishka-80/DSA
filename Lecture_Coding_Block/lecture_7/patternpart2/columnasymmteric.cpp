c62d87586a029f800f1fc6c79c71833ae60c912e
kanishka-80 <kanishkagupta0802@gmail.com>
2026-02-05 08:41:22 +0530

    done

--- Lecture_Coding_Block/lecture_7/patternpart2/columnasymmteric.cpp
+++ Lecture_Coding_Block/lecture_7/patternpart2/columnasymmteric.cpp
@@ -21,21 +21,21 @@ int main(){
         }
         cout<<endl;
      }
-     for (int i=n-1;i>=1;i--){
-         for(int sp=1;sp<=n-i;sp++){
-            cout<<"  ";
-         }
-            for(int j=1;j<=i;j++){
-                cout<<"* ";
-         }
+    //  for (int i=n-1;i>=1;i--){
+    //      for(int sp=1;sp<=n-i;sp++){
+    //         cout<<"  ";
+    //      }
+    //         for(int j=1;j<=i;j++){
+    //             cout<<"* ";
+    //      }
         
-        for(int k=1;k<i;k++){
-            cout<<"* ";
-        }
-        for(int l=1;l<=n-i;l++){
-            cout<<"  ";
-        }
-        cout<<endl;
-     }
+    //     for(int k=1;k<i;k++){
+    //         cout<<"* ";
+    //     }
+    //     for(int l=1;l<=n-i;l++){
+    //         cout<<"  ";
+    //     }
+    //     cout<<endl;
+    //  }
     return 0;
     }
