0c0a4a7f1cdbe2932c9ebd8c9e97fadc53f6a984
kanishka-80 <kanishkagupta0802@gmail.com>
2026-02-09 14:35:37 +0530

    done

--- Lecture_Coding_Block/lecture_18/array_basic.cpp
+++ Lecture_Coding_Block/lecture_18/array_basic.cpp
@@ -7,6 +7,12 @@ int main(){
     cin>>n;
     //2nd way
     int arr[5];
+    arr[0]={1};
+    arr[1]={1};
+    arr[2]={1};
+    arr[3]={1};
+    arr[4]={1};
+
     //3rd way
     int arr_b[]={1,3,2,4};
  
