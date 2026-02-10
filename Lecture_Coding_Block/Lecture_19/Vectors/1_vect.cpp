076e4e64652ef8a71b775ba0b83d8f9bc4438ac8
kanishka-80 <kanishkagupta0802@gmail.com>
2026-02-10 15:54:06 +0530

    done

---
+++ Lecture_Coding_Block/Lecture_19/Vectors/1_vect.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    // initialization
+    vector<int> a;
+    a.push_back(1);
+    a.push_back(2);
+    a.push_back(3);
+    a.push_back(4);
+    a.push_back(5);
+    // print capacity and size
+    cout<<"size"<<a.size()<<endl;
+    cout<<"capacity"<<a.capacity()<<endl;
+    // to delete
+    a.pop_back();
+    cout<<"size"<<a.size()<<endl;
+    cout<<"capacity"<<a.capacity()<<endl;
+    
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
+    //FIRST element
+    cout<<"First element"<<a.front()<<endl;
+    //LAST element
+    cout<<"Last element"<<a.back()<<endl;
+    return 0;
+}
