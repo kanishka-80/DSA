196c4d3987990f3b786785cfc1c79bdd2665f072
kanishka-80 <kanishkagupta0802@gmail.com>
2026-02-14 03:06:43 +0530

    done

--- Assignment/Assignment_3/tempCodeRunnerFile.cpp
+++ Assignment/Assignment_3/tempCodeRunnerFile.cpp
@@ -2,18 +2,23 @@
 using namespace std;
 
 int main(){
+
+    while(true){
     int n;
     cin>>n;
-    int arr[n];
+    int arr[n],sum=0;
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
     for(int i=0;i<n;i++){
-        if(arr[i]<0){
+        sum=sum+arr[i];
+        if(sum<0){
             break;
         }
         else{
+            
             cout<<arr[i]<<endl;
         }
     }
+}
 }
