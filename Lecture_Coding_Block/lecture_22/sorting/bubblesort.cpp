85fd6a3bbbf09ca294f5b21859dca012397bf959
kanishka-80 <kanishkagupta0802@gmail.com>
2026-02-18 14:22:54 +0530

    done

--- Lecture_Coding_Block/lecture_22/sorting/bubblesort.cpp
+++ Lecture_Coding_Block/lecture_22/sorting/bubblesort.cpp
@@ -10,8 +10,8 @@ int main(){
     }
     //bubble sort
     for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-1-i;j++){
-           if(arr[j]>arr[i]){
+        for(int j=0;j<n-i-1;j++){
+           if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
             }
         }
