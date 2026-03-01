75e1b8d3f23c249372529f227df8ffbca22148df
kanishka-80 <kanishkagupta0802@gmail.com>
2026-03-01 19:57:21 +0530

    done

--- Leetcode/48_ques.cpp
+++ Leetcode/48_ques.cpp
@@ -1,5 +1,6 @@
-class Solution {
-public:
+#include <iostream>
+#include <vector>
+using namespace std;
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
@@ -19,4 +20,13 @@ public:
             }
         }
     }
-};
+int main(){
+    vector<vector<int>> matrix={{1,2,3},{4,5,6},{7,8,9}};
+    rotate(matrix);     
+    for(int i=0;i<matrix.size();i++){
+        for(int j=0;j<matrix[0].size();j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }    return 0;
+}
