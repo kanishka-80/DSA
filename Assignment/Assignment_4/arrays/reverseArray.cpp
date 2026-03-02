25aff0719451d8bd667f59e73a4474ddb14ff6f8
kanishka-80 <kanishkagupta0802@gmail.com>
2026-03-02 12:23:16 +0530

    done

--- Assignment/Assignment_4/arrays/reverseArray.cpp
+++ Assignment/Assignment_4/arrays/reverseArray.cpp
@@ -1,21 +1,21 @@
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int ReverseArray(int arr[], int n){
-//     for(int i=0;i<n/2;i++){
-//         swap(arr[i],arr[n-i-1]);
-//     }
-//     return 0;
-// }
-// int main(){
-//     int n;
-//     cin>>n;
-//     int arr[n];
-//     for(int i=0;i<n;i++){
-//         cin>>arr[i];
-//     }
-//     ReverseArray(arr,n);
-//     for(int i=0;i<n;i++){
-//         cout<<arr[i]<<" ";
-//     }
-// }
+int ReverseArray(int arr[], int n){
+    for(int i=0;i<n/2;i++){
+        swap(arr[i],arr[n-i-1]);
+    }
+    return 0;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    ReverseArray(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
