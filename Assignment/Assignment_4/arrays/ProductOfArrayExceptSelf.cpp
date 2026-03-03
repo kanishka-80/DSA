04a97ef1000f49dbb02771e6f0ffdf78b922f0b9
kanishka-80 <kanishkagupta0802@gmail.com>
2026-03-03 21:10:51 +0530

    done

--- Assignment/Assignment_4/arrays/ProductOfArrayExceptSelf.cpp
+++ Assignment/Assignment_4/arrays/ProductOfArrayExceptSelf.cpp
@@ -1,20 +1,20 @@
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cin>>n;
-//     int arr[n];
-//     int output[n];
-//     // int output[0]=1;
-//     for(int i=0;i<n;i++){
-//         cin>>arr[i];
-//     }
-//     for(int i=0;i<n;i++){
-//         for(int j=i+1;j<n;j++){
-//             output[i]*=arr[j];
-//         }
-//     cout<<output[i];
-//     }
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    int output[n];
+    // int output[0]=1;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            output[i]*=arr[j];
+        }
+    cout<<output[i];
+    }
 
-// }
+}
