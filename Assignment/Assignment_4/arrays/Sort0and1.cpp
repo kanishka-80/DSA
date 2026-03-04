e200b39197ad01583a8fa9b2a9e244d64af18870
kanishka-80 <kanishkagupta0802@gmail.com>
2026-03-04 16:15:53 +0530

    done

--- Assignment/Assignment_4/arrays/Sort0and1.cpp
+++ Assignment/Assignment_4/arrays/Sort0and1.cpp
@@ -1,29 +1,29 @@
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cin >> n;
+int main(){
+    int n;
+    cin >> n;
 
-//     int arr[n];
+    int arr[n];
 
-//     for(int i = 0; i < n; i++){
-//         cin >> arr[i];
-//     }
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
 
-//     // Bubble Sort
-//     for(int i = 0; i < n-1; i++){
-//         for(int j = 0; j < n-i-1; j++){
-//             if(arr[j] > arr[j+1]){
-//                 swap(arr[j], arr[j+1]);
-//             }
-//         }
-//     }
+    // Bubble Sort
+    for(int i = 0; i < n-1; i++){
+        for(int j = 0; j < n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
 
-//     // Print sorted array
-//     for(int i = 0; i < n; i++){
-//         cout << arr[i] << " ";
-//     }
+    // Print sorted array
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
 
-//     return 0;
-// }
+    return 0;
+}
