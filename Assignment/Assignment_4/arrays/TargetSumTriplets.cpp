ea3cf765f8f93bdd4a6c0bca2ad6943b1dac7a8e
kanishka-80 <kanishkagupta0802@gmail.com>
2026-03-05 20:11:56 +0530

    done

--- Assignment/Assignment_4/arrays/TargetSumTriplets.cpp
+++ Assignment/Assignment_4/arrays/TargetSumTriplets.cpp
@@ -1,28 +1,28 @@
-// #include<iostream>
-// #include<algorithm>
-// using namespace std;
+#include<iostream>
+#include<algorithm>
+using namespace std;
 
-// int targetSum(int arr[], int n, int target){
-//     sort(arr, arr + n);
-//     for(int i=0;i<n;i++){
-//         for(int j=i+1;j<n;j++){
-//             for(int k=j+1;k<n;k++){
-//              if(arr[i]+arr[j]+arr[k]==target){ 
-//             cout<<arr[i]<<","<<arr[j]<<" and "<<arr[k]<<endl;
-//             }
-//             }
-//         }
-//     }
-//     return 0;
-// }
-// int main(){
-//     int n,target;
-//     cin>>n;
-//     int arr[n];
-//     for(int i=0;i<n;i++){
-//         cin>>arr[i];
-//     }
-//     cin>>target;
-//     targetSum(arr,n,target);
-//     return 0;
-// }
+int targetSum(int arr[], int n, int target){
+    sort(arr, arr + n);
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            for(int k=j+1;k<n;k++){
+             if(arr[i]+arr[j]+arr[k]==target){ 
+            cout<<arr[i]<<","<<arr[j]<<" and "<<arr[k]<<endl;
+            }
+            }
+        }
+    }
+    return 0;
+}
+int main(){
+    int n,target;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cin>>target;
+    targetSum(arr,n,target);
+    return 0;
+}
