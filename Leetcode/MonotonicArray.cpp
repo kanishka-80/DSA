c1c63f6fb2ca137ded08f3e2845ebfab2c211578
kanishka-80 <kanishkagupta0802@gmail.com>
2026-03-09 01:28:26 +0530

    done

---
+++ Leetcode/MonotonicArray.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+    bool isMonotonic(vector<int>& nums) {
+        int n = nums.size();
+        
+        bool increasing = true;
+        bool decreasing = true;
+
+        for(int i = 1; i < n; i++) {
+            
+            if(nums[i] > nums[i - 1]) {
+                decreasing = false;
+            }
+            
+            if(nums[i] < nums[i - 1]) {
+                increasing = false;
+            }
+        }
+
+        return increasing || decreasing;
+    }
