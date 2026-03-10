4243a5bec91f4fef7f1a7baefb25f299722a4d58
kanishka-80 <kanishkagupta0802@gmail.com>
2026-03-10 23:15:41 +0530

    done

---
+++ Leetcode/ReshapeMatrix.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+    int n = mat.size();
+    int m = mat[0].size();
+    if(n*m != r*c){
+    return mat;    
+    }
+    vector<vector<int>> ans(r, vector<int>(c));
+    int x=0, y=0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            ans[x][y] = mat[i][j];
+            y++;
+            if(y==c){
+                x++;
+                y=0;
+            }
+        }
+    }
+    return ans;
+    }
