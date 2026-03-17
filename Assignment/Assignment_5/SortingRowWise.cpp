7c3404cc6d4b49e2c09d07b6b2f16b28ad945fb7
kanishka-80 <kanishkagupta0802@gmail.com>
2026-03-17 20:56:15 +0530

    done

--- Assignment/Assignment_5/SortingRowWise.cpp
+++ Assignment/Assignment_5/SortingRowWise.cpp
@@ -1,30 +1,30 @@
-// #include<iostream>
-// #include<algorithm>
-// using namespace std;
+#include<iostream>
+#include<algorithm>
+using namespace std;
 
-// int main(){
-//     int r,c;
-//     cin>>r>>c;
-//     int arr[r][c];
-//     for(int i=0;i<r;i++){
-//         for(int j=0;j<c;j++){
-//             cin>>arr[i][j];
-//         }
-//     }
-//     for(int i=0;i<r;i++){
-//         for(int k=0;k<c-1;k++){
-//              for(int j=0;j<c-1;j++){
-//                 if(arr[i][j]>arr[i][j+1]){
-//                   swap(arr[i][j],arr[i][j+1]);    
-//                 }
-//             }
-//         }
-//     }
-//     for(int i=0;i<r;i++){
-//         for(int j=0;j<c;j++){
-//             cout<<arr[i][j]<<" ";
-//         }
-//         cout<<endl;
-//     }
+int main(){
+    int r,c;
+    cin>>r>>c;
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<r;i++){
+        for(int k=0;k<c-1;k++){
+             for(int j=0;j<c-1;j++){
+                if(arr[i][j]>arr[i][j+1]){
+                  swap(arr[i][j],arr[i][j+1]);    
+                }
+            }
+        }
+    }
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 
-// }
+}
