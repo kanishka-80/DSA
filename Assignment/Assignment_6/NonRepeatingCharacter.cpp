8de2c3d26b32ab5d70fd47c39390a6acbfaadf97
kanishka-80 <kanishkagupta0802@gmail.com>
2026-03-21 00:18:46 +0530

    done

--- Assignment/Assignment_6/NonRepeatingCharacter.cpp
+++ Assignment/Assignment_6/NonRepeatingCharacter.cpp
@@ -1,30 +1,30 @@
-// #include<iostream>
-// #include<string>
-// using namespace std;
+#include<iostream>
+#include<string>
+using namespace std;
 
-// int main(){
-//     int T;
-//     cin>>T;
-//     while(T--){
-//         string s;
-//         cin>>s;
-//         int freq[256]={0};
-//         for(int i=0;i<s.length();i++){
-//            freq[s[i]]++;
-//         }
-//         char ans='-';
-//         for(int i=0;i<s.length();i++){
-//             if(freq[s[i]]==1){
-//                 ans=s[i];
-//                 break;
-//             }
-//         }
-//     if(ans == '-'){
-//         cout<<-1;
-//     }
-//     else{
-//         cout<<ans;
-//     }
-// }
-// return 0;
-// }
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        string s;
+        cin>>s;
+        int freq[256]={0};
+        for(int i=0;i<s.length();i++){
+           freq[s[i]]++;
+        }
+        char ans='-';
+        for(int i=0;i<s.length();i++){
+            if(freq[s[i]]==1){
+                ans=s[i];
+                break;
+            }
+        }
+    if(ans == '-'){
+        cout<<-1;
+    }
+    else{
+        cout<<ans;
+    }
+}
+return 0;
+}
