230d55efc4a262eb798ea785acdb89b678133eec
kanishka-80 <kanishkagupta0802@gmail.com>
2026-03-20 18:58:51 +0530

    done

--- Assignment/Assignment_6/1_CanYouReadThis.cpp
+++ Assignment/Assignment_6/1_CanYouReadThis.cpp
@@ -3,5 +3,22 @@
 using namespace std;
 
 int main(){
-    
+    string s;
+    cin>>s;
+    string word="";
+
+    for(int i=0;i<s.length();i++){
+        if(isupper(s[i] && word!="")){
+            cout<<word<<endl;
+            word=s[i];
+        }
+        else{
+            word+=s[i];
+        }
+    }
+    if(word != ""){
+        cout<<word<<endl;
+    }
+    return 0;
+
 }
