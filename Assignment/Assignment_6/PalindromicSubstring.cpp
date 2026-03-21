1c7f52bcc02f2b751b22c749f78cdb4f12187fe4
kanishka-80 <kanishkagupta0802@gmail.com>
2026-03-22 00:20:15 +0530

    done

--- Assignment/Assignment_6/PalindromicSubstring.cpp
+++ Assignment/Assignment_6/PalindromicSubstring.cpp
@@ -1,19 +1,19 @@
-// #include<iostream>
-// #include<string>
-// using namespace std;
+#include<iostream>
+#include<string>
+using namespace std;
 
-// int main(){
-//     string s;
-//     cin>>s;
+int main(){
+    string s;
+    cin>>s;
 
-//     int st=0;
-//     int end=s.length()-1;
-//     while(st<end){
-//         if(s[st]!=s[end]){
-//             st++;
-//             end--;
-//         }
-//     }
+    int st=0;
+    int end=s.length()-1;
+    while(st<end){
+        if(s[st]!=s[end]){
+            st++;
+            end--;
+        }
+    }
 
     
-// }
+}
