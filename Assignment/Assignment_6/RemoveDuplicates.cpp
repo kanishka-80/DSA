7607ad0364425855716cee7e4dc8ff0f98617b53
kanishka-80 <kanishkagupta0802@gmail.com>
2026-03-22 00:21:22 +0530

    done

--- Assignment/Assignment_6/RemoveDuplicates.cpp
+++ Assignment/Assignment_6/RemoveDuplicates.cpp
@@ -1,16 +1,16 @@
-// #include<iostream>
-// #include<string>
-// using namespace std;
+#include<iostream>
+#include<string>
+using namespace std;
 
-// int main(){
-//     string s;
-//     cin>>s;
-//     string result="";
-//     result+=s[0];
-//     for(int i=1;i<s.length();i++){
-//         if(s[i]!=s[i-1]){
-//             result+=s[i];
-//         }
-//     }
-//     cout<<result;
-// }
+int main(){
+    string s;
+    cin>>s;
+    string result="";
+    result+=s[0];
+    for(int i=1;i<s.length();i++){
+        if(s[i]!=s[i-1]){
+            result+=s[i];
+        }
+    }
+    cout<<result;
+}
