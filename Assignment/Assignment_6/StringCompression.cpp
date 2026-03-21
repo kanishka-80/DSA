f1e569f9964c2fb53b901e7a6656b736a2ab6d02
kanishka-80 <kanishkagupta0802@gmail.com>
2026-03-21 19:47:14 +0530

    done

--- Assignment/Assignment_6/StringCompression.cpp
+++ Assignment/Assignment_6/StringCompression.cpp
@@ -2,26 +2,21 @@
 #include<string>
 using namespace std;
 
-int main(){
-    string S;
-    cin>>S;
-    int c_a=0,c_b=0,c_c=0,c_d=0,c_s=0;
-    for(int i=0;i<S.length();i++){
-        if(S[i] =='a'){
-            c_a++;
-        }
-        else if(S[i]=='b'){
-            c_b++;
-        }
-        else if(S[i]=='c'){
-            c_c++;
-        }
-        else if(S[i]=='d'){
-            c_d++;
-        }
-        else if(S[i]=='s'){
-            c_s++;
+int main() {
+    string s;
+    cin >> s;
+
+    int count = 0;
+
+    for (int i = 0; i < s.length()-1; i++) {
+        if (i < s.length() - 1 && s[i] == s[i + 1]) {
+            count++;
+        } 
+        else {
+            cout << s[i] << count;
+            count = 1;
         }
     }
-    cout<<'a'<<c_a<<'b'<<c_b<<'c'<<c_c<<'d'<<c_d<<'s'<<c_s;
+
+    return 0;
 }
