ec3547490a3010566195cd42bb7088400a5d7454
kanishka-80 <kanishkagupta0802@gmail.com>
2026-03-23 00:18:48 +0530

    done

--- Assignment/Assignment_6/MaxFrequencyCharacter.cpp
+++ Assignment/Assignment_6/MaxFrequencyCharacter.cpp
@@ -1,26 +1,26 @@
-// #include<iostream>
-// #include<string>
-// using namespace std;
+#include<iostream>
+#include<string>
+using namespace std;
 
-// int main(){
-//     string s;
-//     cin>>s;
+int main(){
+    string s;
+    cin>>s;
 
-//     int freq[256]={0};
-//     for(int i=0;i<s.length();i++){
-//             freq[s[i]]++;
-//     }
-//     int maxFreq = 0;
-//     char ans;
+    int freq[256]={0};
+    for(int i=0;i<s.length();i++){
+            freq[s[i]]++;
+    }
+    int maxFreq = 0;
+    char ans;
 
-//     for(int i = 0; i < 256; i++){
-//         if(freq[i] > maxFreq){
-//             maxFreq = freq[i];
-//             ans = char(i);
-//         }
-//     }
+    for(int i = 0; i < 256; i++){
+        if(freq[i] > maxFreq){
+            maxFreq = freq[i];
+            ans = char(i);
+        }
+    }
 
-//     cout << ans << " " << maxFreq;
+    cout << ans << " " << maxFreq;
 
-//     return 0;
-// }
+    return 0;
+}
