be031b9dde5039017bbeff3bde550a5978e98505
kanishka-80 <kanishkagupta0802@gmail.com>
2026-03-23 22:51:42 +0530

    done

--- Assignment/Assignment_5/MatrixSearch.cpp
+++ Assignment/Assignment_5/MatrixSearch.cpp
@@ -1,23 +1,23 @@
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int main(){
-//     int M,N;
-//     cin>>M>>N;
-//     int arr[30][30];
-//     int x;
-//     cin>>x;
-//     for(int i=0;i<M;i++){
-//         for(int j=0;j<N;j++){
-//             cin>>arr[i][j];
-//         }
-//         for(int i=0;i<M;i++){
-//             for(int j=0;j<N;j++){
-//                 if(arr[i][j]==x){
-//                     cout<<x;
-//                 }
-//             }
-//         }
+int main(){
+    int M,N;
+    cin>>M>>N;
+    int arr[30][30];
+    int x;
+    cin>>x;
+    for(int i=0;i<M;i++){
+        for(int j=0;j<N;j++){
+            cin>>arr[i][j];
+        }
+        for(int i=0;i<M;i++){
+            for(int j=0;j<N;j++){
+                if(arr[i][j]==x){
+                    cout<<x;
+                }
+            }
+        }
         
-//     }
-// }
+    }
+}
