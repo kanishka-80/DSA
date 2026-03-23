c789691f63ac8f6417dc1218713b030e1273f479
kanishka-80 <kanishkagupta0802@gmail.com>
2026-03-23 22:54:06 +0530

    done

--- Assignment/Assignment_6/StringOddEvenCharacter.cpp
+++ Assignment/Assignment_6/StringOddEvenCharacter.cpp
@@ -1,18 +1,18 @@
-// #include<iostream>
-// #include<string>
-// using namespace std;
+#include<iostream>
+#include<string>
+using namespace std;
 
-// int main(){
-//     string s;
-//     cin>>s;
+int main(){
+    string s;
+    cin>>s;
 
-//     for(int i=0;i<s.length();i++){
-//         if(i%2==0){
-//             (int)s[i]++;
-//         }
-//         else{
-//             (int)s[i]--;
-//         }
-//     }
-//     cout<<s;
-// }
+    for(int i=0;i<s.length();i++){
+        if(i%2==0){
+            (int)s[i]++;
+        }
+        else{
+            (int)s[i]--;
+        }
+    }
+    cout<<s;
+}
