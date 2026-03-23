2a060b1e3825dc5e7e2d24104f5f98399df6214d
kanishka-80 <kanishkagupta0802@gmail.com>
2026-03-23 22:53:20 +0530

    done

--- Assignment/Assignment_6/ToggleCase.cpp
+++ Assignment/Assignment_6/ToggleCase.cpp
@@ -1,22 +1,22 @@
-// #include<iostream>
-// #include<string>
-// using namespace std;
+#include<iostream>
+#include<string>
+using namespace std;
 
-// int main(){
-//     string s;
-//     cin>>s;
+int main(){
+    string s;
+    cin>>s;
 
-//     for(int i=0;i<s.length();i++){
-//          for(int i = 0; i < s.length(); i++){
-//         if(islower(s[i])){
-//             s[i] = toupper(s[i]);
-//         }
-//         else if(isupper(s[i])){
-//             s[i] = tolower(s[i]);
-//         }
-//     }
+    for(int i=0;i<s.length();i++){
+         for(int i = 0; i < s.length(); i++){
+        if(islower(s[i])){
+            s[i] = toupper(s[i]);
+        }
+        else if(isupper(s[i])){
+            s[i] = tolower(s[i]);
+        }
+    }
 
-//     cout << s;
-//     return 0;
-//     }
-// }
+    cout << s;
+    return 0;
+    }
+}
