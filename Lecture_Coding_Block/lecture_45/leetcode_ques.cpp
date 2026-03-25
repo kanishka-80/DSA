96a6a2115cf375d18889343f63a0b48fe0375cac
kanishka-80 <kanishkagupta0802@gmail.com>
2026-03-25 20:53:04 +0530

    done

---
+++ Lecture_Coding_Block/lecture_45/leetcode_ques.cpp
@@ -0,0 +1,3 @@
+// 643 - Maximum Average Subarray
+// 1343 - Number of Sub-array of size K and Average Greater than or Equal to threshold
+// 1456 - Maximum number of vowels in a substring of Given length
