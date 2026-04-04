81c03b9e74b206bbe1b0b27c6932a98b06bb4079
kanishka-80 <kanishkagupta0802@gmail.com>
2026-04-04 20:13:17 +0530

    done

--- Lecture_Coding_Block/lecture_48/35leetcode_ques.cpp
+++ Lecture_Coding_Block/lecture_48/35leetcode_ques.cpp
@@ -15,7 +15,7 @@ int main(){
     while(start <= end){
         int mid=start+(end-start)/2;
         if(a[mid]==target){
-            return mid;
+            cout<<mid;
         }
         if(a[mid]<target){
             start=mid+1;
